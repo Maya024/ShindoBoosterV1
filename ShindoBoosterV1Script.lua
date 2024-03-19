057b0f1545c39f4f9bffb62386dfe81653a60d4a
Maya024 <163868808+Maya024@users.noreply.github.com>
2024-03-19 23:20:30 +0100

    Update ShindoBoosterV1Script.lua

--- ShindoBoosterV1Script.lua
+++ ShindoBoosterV1Script.lua
@@ -1,3 +1,12 @@
+--[[
+
+ShindoBoosterV1 Interface Suite
+by Mays (Maya)
+
+Mays (Maya) | Designing + Programming
+
+]]
+
 local function createPlayerMenu(players)
     local playerDropdown = Instance.new("ScreenGui")
     playerDropdown.Name = "PlayerDropdownGui"
