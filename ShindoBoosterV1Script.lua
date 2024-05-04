3d4a22748bafbd85dd3d7ccd8a6eac81069fe665
Maya <163868808+Maya024@users.noreply.github.com>
2024-05-04 18:14:02 +0200

    Update ShindoBoosterV1Script.lua

--- ShindoBoosterV1Script.lua
+++ ShindoBoosterV1Script.lua
@@ -115,80 +115,41 @@ local function createPlayerMenu(players)
             playerButton.TextColor3 = Color3.fromRGB(255, 255, 255) 
             playerButton.Text = player.Name
             playerButton.Parent = playerListScrollingFrame
-    
+
             playerButton.MouseButton1Click:Connect(function()
                 playerButton.Visible = false
                 getgenv().Nickname = player.Name
-                updatePlayerMovement(player, true)
-            
-                local allJutsu = game:GetService("ReplicatedStorage"):WaitForChild("alljutsu")
-                if allJutsu:FindFirstChild("RELL") then
-                    workspace[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = workspace[Nickname].HumanoidRootPart.CFrame
-                    game:GetService("Players").LocalPlayer.startevent:FireServer("band", "\128")
-                    local args = {
-                        [1] = "equipjutsu",
-                        [2] = "v",
-                        [3] = allJutsu.RELL:WaitForChild("rell1")
-                    }
-            
-                    game:GetService("Players").LocalPlayer:WaitForChild("startevent"):FireServer(unpack(args))
-                    local args = {
-                        [1] = "key",
-                        [2] = "v"
-                    }
-            
-                    game:GetService("Players").LocalPlayer.Character.combat.update:FireServer(unpack(args))
-                    local args = {
-                        [1] = "key",
-                        [2] = "vend"
-                    }
-            
-                    game:GetService("Players").LocalPlayer.Character.combat.update:FireServer(unpack(args))
-                    wait(4)
-                    updatePlayerMovement(player, false)
-                    wait(6)
-                    local TeleportService = game:GetService("TeleportService")
-                    TeleportService:Teleport(game.PlaceId, game.Players.LocalPlayer)
-                else
-                    local noRellFrame = Instance.new("Frame")
-                    noRellFrame.Size = UDim2.new(0.5, 0, 0.5, 0)
-                    noRellFrame.Position = UDim2.new(0.5, 0, 0.5, 0)
-                    noRellFrame.AnchorPoint = Vector2.new(0.5, 0.5)
-                    noRellFrame.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-                    noRellFrame.BorderSizePixel = 0
-                    noRellFrame.Parent = playerDropdownFrame
-            
-                    local noRellText = Instance.new("TextLabel")
-                    noRellText.Size = UDim2.new(1, 0, 0.8, 0)
-                    noRellText.Position = UDim2.new(0, 0, 0.1, 0)
-                    noRellText.BackgroundTransparency = 1
-                    noRellText.Font = Enum.Font.SourceSansBold
-                    noRellText.TextSize = 24
-                    noRellText.TextColor3 = Color3.fromRGB(255, 255, 255)
-                    noRellText.Text = "You do not have the 'RELL' Bloodline or the first move."
-                    noRellText.Parent = noRellFrame
-            
-                    local okButton = Instance.new("TextButton")
-                    okButton.Size = UDim2.new(0.6, 0, 0.1, 0)
-                    okButton.Position = UDim2.new(0.5, -okButton.Size.X.Offset / 2, 0.9, 0)
-                    okButton.AnchorPoint = Vector2.new(0.5, 0)
-                    okButton.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-                    okButton.BorderSizePixel = 0
-                    okButton.Font = Enum.Font.SourceSansBold
-                    okButton.TextSize = 20
-                    okButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-                    okButton.Text = "Okay"
-                    okButton.Parent = noRellFrame
-            
-                    okButton.MouseButton1Click:Connect(function()
-                        noRellFrame:Destroy()
-                    end)
-                end
-            end)            
+
+                workspace[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = workspace[Nickname].HumanoidRootPart.CFrame
+                game:GetService("Players").LocalPlayer.startevent:FireServer("band", "\128")
+                local args = {
+                    [1] = "equipjutsu",
+                    [2] = "v",
+                    [3] = game:GetService("ReplicatedStorage"):WaitForChild("alljutsu"):WaitForChild("RELL"):WaitForChild("rell1")
+                }
+
+                game:GetService("Players").LocalPlayer:WaitForChild("startevent"):FireServer(unpack(args))
+                local args = {
+                    [1] = "key",
+                    [2] = "v"
+                }
+
+                game:GetService("Players").LocalPlayer.Character.combat.update:FireServer(unpack(args))
+                local args = {
+                    [1] = "key",
+                    [2] = "vend"
+                }
+
+                game:GetService("Players").LocalPlayer.Character.combat.update:FireServer(unpack(args))
+                wait(2)
+                local TeleportService = game:GetService("TeleportService")
+                TeleportService:Teleport(game.PlaceId, game.Players.LocalPlayer)
+            end)
         end
-    end    
+    end
 
     -- Functions for moving the UI
+ 
     local dragging
     local dragInput
     local dragStart
@@ -228,6 +189,7 @@ end
 
 
 -- Main script
+
 getgenv().toggle25 = true
 getgenv().Nickname = "PlayerName"
 repeat wait() until game:GetService("Players").PlayerAdded and getgenv().toggle25 == true
